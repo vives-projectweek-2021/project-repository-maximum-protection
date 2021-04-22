7725f347de31052bb1748cc6adeb2a9222d0f844
Robin Debel <robindebel@hotmail.nl>
2021-04-22 09:01:32 +0200

    fireballs kill again

--- src/game.ts
+++ src/game.ts
@@ -376,7 +376,7 @@ export default class Game extends Phaser.Scene {
         fireball = this.physics.add.sprite(dragon.x,player.y-1200,'fireball1').setScale(0.5).refreshBody()
         fireball.play('movingfireball',true)
         fireball.body.setMaxVelocityY(velocityfireball)
-        //this.physics.add.overlap(player,fireball,hitFireball,null,this);
+        this.physics.add.overlap(player,fireball,hitFireball,null,this);
 
         //camera settings
         this.cameras.main.startFollow(player)
